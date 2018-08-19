40410a9b6eb7099e5865ece2c58c4cd8afdf8c6a
niexuecheng <niexuecheng@gmail.com>
2018-08-19 17:24:02 +0800

    First Commit

--- run_train.sh
+++ run_train.sh
@@ -1,5 +1,5 @@
  CUDA_VISIBLE_DEVICES=0,1 python main.py \
-                          -b 45 \
+                          -b 30 \
                           --epochs 250 \
                           --lr 0.003 \
                           --snapshot-fname-prefix exps/snapshots/ppn \
